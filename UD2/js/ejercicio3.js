57e4385fe6073419457202166e3c95d3db1d84b5
Pablo Tabernero <a22pablotv@iessanclemente.net>
2023-11-01 21:29:47 +0100

    Hecho Ejercicio3 Actividade1

---
+++ UD2/js/ejercicio3.js
@@ -0,0 +1,19 @@
+//Operación 125 / 8
+let numero = 125;
+let resultado = numero >> 3;
+console.log(resultado);
+
+//Operación 40 x 4
+numero = 40;
+resultado = numero << 2;
+console.log(resultado);
+
+//Operación 25 x 2
+numero = 25;
+resultado = 25 >> 1;
+console.log(resultado);
+
+//Operación 10 x 16
+numero = 10;
+resultado = numero << 4;
+console.log(resultado);

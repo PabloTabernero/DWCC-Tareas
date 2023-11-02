5e978c952289f6813f9d70cf9b334ecb48ba96b1
Pablo Tabernero <a22pablotv@iessanclemente.net>
2023-11-02 12:03:39 +0100

    Ultima correccion

--- UD2/js/ejercicio1.js
+++ UD2/js/ejercicio1.js
@@ -1,15 +1,12 @@
 //Ejercicio que muestra la tabla de multiplicar del 9 con bucle while.
 console.log(`Tabla de Multiplicar del 9`);
-
 let i = 1;
 while (i < 11) {
     console.log(`9 x ${i} = ` + (i*9));
     i++;
 }
-
 //Ejercicio que muestra los 10 primeros numeros Fibonacci con bucle for.
 console.log(`Numeros Fibonacci`);
-
 let a = 0;
 let b = 1;
 console.log(a);
@@ -21,12 +18,9 @@ for(let i = 2; i < 10; i++) {
     a = b;
     b = siguiente;
 }
-
 //Ejercicio que muestra si un número es primo.
 console.log(`Numero Primo`);
-
 let primo = parseInt(prompt("Por favor, introduce un numero (0 para terminar):"));
-
 if (primo < 2) {
     console.log(`El numero ${primo} no es primo.`);
 }else if (primo < 4) {
--- UD2/ts/ejercicio1.ts
+++ UD2/ts/ejercicio1.ts
@@ -1,6 +1,5 @@
 //Ejercicio que muestra la tabla de multiplicar del 9 con bucle while.
 console.log(`Tabla de Multiplicar del 9`);
-
 let i: number = 1;
 while (i < 11) {
     console.log(`9 x ${i} = ` + (i*9));
@@ -9,12 +8,10 @@ while (i < 11) {
 
 //Ejercicio que muestra los 10 primeros numeros Fibonacci con bucle for.
 console.log(`Numeros Fibonacci`);
-
 let a: number = 0;
 let b: number = 1;
 console.log(a);
 console.log(b);
-
 for(let j: number = 2; j < 10; j++) {
     let siguiente: number = a + b;
     console.log(siguiente);
@@ -24,9 +21,7 @@ for(let j: number = 2; j < 10; j++) {
 
 //Ejercicio que muestra si un número es primo.
 console.log(`Numero Primo`);
-
 let primo: number | null = parseInt(prompt("Por favor, introduce un numero (0 para terminar):"));
-
 if (primo < 2) {
     console.log(`El numero ${primo} no es primo.`);
 }else if (primo < 4) {

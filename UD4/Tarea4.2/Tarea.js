2c0a6f630bbd7e63cf35aee4ad5fd0050bb1dadd
Pablo Tabernero <a22pablotv@iessanclemente.net>
2024-01-18 00:23:26 +0100

    Inicio Tarea Objetos

---
+++ UD4/Tarea4.2/Tarea.js
@@ -0,0 +1,50 @@
+class Disco {
+    nombreDisco;
+    grupo;
+    anoPublicacion;
+    tipoMusica;
+    prestado;
+
+    constructor(nombreDisco, grupo, anoPublicacion, tipoMusica, prestado) {
+        this.nombreDisco = nombreDisco;
+        this.grupo = grupo;
+        this.anoPublicacion = anoPublicacion;
+        this.tipoMusica = tipoMusica;
+        this.prestado = prestado;
+    }
+
+    cargarDatos(nombreDisco, grupo, anoPublicacion, tipoMusica, prestado) {
+        this.nombreDisco = nombreDisco;
+        this.grupo = grupo;
+        this.anoPublicacion = anoPublicacion;
+        this.tipoMusica = tipoMusica;
+        this.prestado = prestado;
+    }
+
+    verInfoDisco() {
+        console.log('Nombre: ' + this.nombreDisco);
+        console.log('Grupo o Solista: ' + this.grupo);
+        console.log('Año Publicación: ' + this.anoPublicacion);
+        console.log('Tipo de Música: ' + this.tipoMusica);
+        console.log('Presatado: ' + this.prestado);
+    }
+
+    static verColeccion(coleccion) {
+        coleccion.forEach(disco => disco.verInfoDisco());
+    } 
+}
+
+//
+let coleccion = [];
+
+let disco1 = new Disco("Master of Puppets", "Metallica", 1986, "Metal", false);
+coleccion.push(disco1);
+
+let disco2 = new Disco("Ride the Lightning", "Metallica", 1984, "Metal", true);
+coleccion.push(disco2);
+
+let disco3 = new Disco()
+disco3.cargarDatos("Metallica (The Black Album)", "Metallica", 1991, "Metal", false);
+coleccion.push(disco3);
+
+Disco.verColeccion(coleccion);
